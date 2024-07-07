7199da8e47275ae1837a60700d506d9f832e5382
Aza-0505 <kobakhidzeaza@gmail.com>
2024-07-08 01:21:46 +0400

    main

---
+++ JS/script.js
@@ -0,0 +1,106 @@
+
+'use strict';
+1.//function
+//function sum(...numbers){
+console.log(x+y);
+sum(10,50,6,7,8,11,6,3,9)}
+
+
+        
+
+    2.let user = {
+        firstname: 'giorgi',
+        lastname: 'saakadze',
+        age: 32,
+        isloggedin: true,
+        if isloggedin = true;{
+            fullName: function() => {
+            return this.firstName + " " + this.lastName;}
+            {if isloggined = false;{
+                 return: false;}
+            }
+            
+          }
+    }
+    3. 
+    let arrayNumbers[3,5,700,50,];
+    function testfnc1(arrayNumbers);{
+        let result1 = SELECT MAX(arraynumbers);
+        FROM arrayNambers; {
+            return: result1;
+    }
+    
+    }
+    4. function testfnc1(x); {
+        if (x question %2 ===0); {
+            return: 'this number is even';
+        }
+        if(x question %2 > 0); {
+            return: 'this number is odd';
+        }
+
+    }
+
+    5. let arrayNumbers= [1,2,3,4,5]; {
+        Reverse the Array
+        let arrayNumbers = numbers.toReversed();
+
+    }
+
+    6. 
+    //arrow function
+    let getAge = (birthYear,yearNow) =>{
+        let age= yearNow-birthYear;
+        if (age > 18); {
+            return 'adult'
+        } else { 'not adult'}
+        yearNow-birthYear;
+    }
+    let result 1 = getAge(2015, 2024);
+        
+    }
+
+    7. let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (const 5 in object) {
+        if (Object.hasOwnProperty.call(object, 5)) {
+            const element = object[5];
+            console.log ('aris');
+            break
+        }
+    }
+
+   8. let array3= [1, 2, 3, 4, 5]; {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[5];
+        console.log(sum);
+    }
+   }
+
+    9. let array3= [1, 2, 3, 4, 5]; {
+        for (let index = 0; index < array.length; index++) {
+            const element = array[5];
+            console.log(sum/length);
+            
+        }
+10. let array5 = [1, 2, 3, 7, 6, 9]; {
+    if(element >7);{
+    console.log (element);}
+    if (element <7);{
+        console.log(element);
+    }
+}
+
+11. 
+let array5 = [32, 14, null, '40', 50,]; {
+    if (element question %5 ===0); {
+        console.log (element);
+    }
+}
+
+12.
+Let array8 = [ [2, -3, 5, 11], [1, -35, –11], [12, -36, -24] ]; {
+    if (element >0); {
+        console.log(element);
+    }
+}
+
